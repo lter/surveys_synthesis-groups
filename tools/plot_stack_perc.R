19e21ffe97221b10e9a6f64b00f95a9fc74cfd52
njlyon0 <nicholasjlyon@gmail.com>
2025-06-06 17:03:45 -0400

    feat (fxn): `plot_stack_perc` now adds the number of total responses to the top of each cohort's bar

--- tools/plot_stack_perc.R
+++ tools/plot_stack_perc.R
@@ -7,11 +7,14 @@
 #' @param colors (character) Vector of colors to map to answer levels. Can be named or unnamed
 #' @param hline_int (NULL / numeric) _Optional_ argument for any horizontal lines to put 'in front of' the bars. Can support any number of numbers if specified (one line created per user-supplied y-intercept)
 #' @param hline_col (NULL / character) _Optional_ argument for coloring horizontal line(s). Useful for making sure line color contrasts nicely with stacked bar colors. Regardless of number of horizontal lines, **this argument only accepts one color**. Defaults to black (specified as a hexadecimal code)
+#' @param total_y (NULL / numeric) _Optional_ argument for specifying vertical position of text identifying survey total (per cohort) Defaults to 90 (90% on Y-axis)
+#' #' @param total_col (NULL / character) _Optional_ argument for specifying color of text including survey total (per cohort.) Defaults to white
 #' 
 #' @return (ggplot2 graph) Finished graph
 #' 
 plot_stack_perc <- function(df = NULL, resp = NULL, colors = NULL, 
-                            hline_int = NULL, hline_col = "#000"){
+                            hline_int = NULL, hline_col = "#000", 
+                            total_y = 90, total_col = "#fff"){
   
   # Errors for 'df'
   if(is.null(df) || "data.frame" %in% class(df) != TRUE || !all(c("cohort", "perc_resp") %in% names(df)))
@@ -35,12 +38,20 @@ plot_stack_perc <- function(df = NULL, resp = NULL, colors = NULL,
       stop("If 'hline_int' is provided, 'hline_col' must be a 1-element character vector")
   }
   
+  # Errors for 'total_y'
+  if(is.null(total_y) || is.numeric(total_y) != TRUE || length(total_y) != 1 || total_y < 0 || total_y > 100)
+    stop("'total_y' must be a single number between 0 and 100")
+  
+  # Errors for 'total_col'
+  if(is.null(total_col) || is.character(total_col) != TRUE || length(total_col) != 1)
+    stop("'total_col' must be a single color, specified as a character")
+  
   # Generate desired plot
   q <- ggplot2::ggplot(data = df, mapping = ggplot2::aes(x = cohort, y = perc_resp, 
                                                          fill = .data[[resp]],
                                                          color = "x")) +
     ggplot2::geom_bar(stat = "identity") +
-    ggplot2::labs(x = "Cohort", y = "Percent of Participants (%)") +
+    ggplot2::labs(x = "Cohort", y = "Percent of Responses (%)") +
     ggplot2::scale_fill_manual(values = colors) +
     ggplot2::scale_color_manual(values = "#000") +
     # Horizontal lines (if provided)
@@ -54,6 +65,30 @@ plot_stack_perc <- function(df = NULL, resp = NULL, colors = NULL,
                    strip.text = ggplot2::element_text(size = 14),
                    text = ggplot2::element_text(size = 14),
                    axis.title = ggplot2::element_text(size = 16))
+  
+  # Add totals per cohort
+  for(focal_cohort in sort(unique(df$cohort))){
+    # focal_cohort <- "2016"
+    
+    # Identify the desired numbers
+    lab <- df %>% 
+      # Count along cohort values
+      dplyr::mutate(cohort_num = as.numeric(cohort)) %>% 
+      # Grab the total responses for that cohort
+      dplyr::filter(cohort == focal_cohort) %>% 
+      dplyr::select(cohort, total_particip, cohort_num) %>% 
+      # Get good X/Y coordinates
+      dplyr::rename(val = total_particip,
+                    x = cohort_num) %>% 
+      dplyr::mutate(y = total_y) %>% 
+      # Driop non-unique rows
+      dplyr::distinct()
+    
+    # Add this to the plot
+    q <- q +
+      ggplot2::geom_text(label = lab$val, y = lab$y, x = lab$x, size = 6, color = total_col)
+    
+  }
   
   # Return created graph
   return(q) }

0ef1d58274f3e536a8e6b9d68daeb9d801dd7d37
njlyon0 <nicholasjlyon@gmail.com>
2025-06-06 10:33:13 -0400

    feat (fxn): adding roxygen-format to `plot_facet` and improving function internals

--- tools/plot_facet.R
+++ tools/plot_facet.R
@@ -1,21 +1,32 @@
-## Creates graph faceted by cohort
-
-# Define function
-plot_facet <- function(data = NULL, ..., color = NULL){
+#' @title Create a Graph Faceted by Cohort
+#' 
+#' @description Creates a graph that is faceted by cohort and has each color given a user-specified color
+#' 
+#' @param df (data.frame-like) Survey data containing--at least--a column named "cohort"
+#' @param colors (character) Vector of colors to map to cohorts. Can be named or unnamed
+#' 
+#' @return (ggplot2 graph) Finished graph
+#' 
+plot_facet <- function(df = NULL, ..., colors = NULL){
   
-  # Error out for missing data / response variable
-  if(is.null(data) == TRUE | is.null(color) == TRUE)
-    stop("All arguments must be specified")
+  # Errors for 'df'
+  if(is.null(df) || !"data.frame" %in% class(df) || !"cohort" %in% names(df))
+    stop("'df' must be data.frame-like and contain a column named 'cohort")
+  
+  # Errors for 'colors'
+  if(is.null(colors) || is.character(colors) != TRUE || length(colors) < length(unique(df$cohort)))
+    stop("'colors' must be provided and contain as many values as there are cohorts")
   
   # Generate barplot faceted by cohort
-  graph <- ggplot(data, aes(..., fill = cohort)) +
-    geom_bar(stat = 'identity') +
-    coord_flip() + 
-    theme(axis.title.x = element_blank()) +
-    facet_grid(cohort ~ .) +
-    scale_fill_manual(values = color) +
-    lno_theme +
-    theme(legend.position = "none")
+  graph <- ggplot2::ggplot(.data = df, mapping = ggplot2::aes(..., fill = cohort)) +
+    ggplot2::geom_bar(stat = 'identity') +
+    ggplot2::coord_flip() + 
+    ggplot2::facet_grid(cohort ~ .) +
+    ggplot2::scale_fill_manual(values = colors) +
+    ggplot2::theme(axis.title.x = ggplot2::element_blank(),
+                   legend.position = "none")
   
   # Return that plot
   return(graph) }
+
+# End ----

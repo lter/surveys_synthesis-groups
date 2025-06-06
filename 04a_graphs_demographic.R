4b9f98ff5609de6de31de38f041ca30433636dce
njlyon0 <nicholasjlyon@gmail.com>
2025-06-06 10:33:58 -0400

    feat (viz demog): fleshed out draft workflow for one graph

--- 04a_graphs_demographic.R
+++ 04a_graphs_demographic.R
@@ -20,21 +20,41 @@ dir.create(file.path("graphs"), showWarnings = F)
 rm(list = ls()); gc()
 
 # Read in the data
-demo_v1 <- read.csv(file = file.path("data", "tidy", "wg-survey-tidy_demographic.csv"))
+demo <- read.csv(file = file.path("data", "tidy", "wg-survey-tidy_demographic.csv")) %>% 
+  # Make cohort a factor with levels
+  dplyr::mutate(cohort = factor(x = cohort, levels = sort(unique(.$cohort))))
 
 # Check structure
-dplyr::glimpse(demo_v1)
+dplyr::glimpse(demo)
 
+# Load needed custom function(s)
+purrr::walk(.x = dir(path = "tools"), .f = ~ source(file.path("tools", .x)))
 
+# Assemble a nice file stem for graphs on this survey's data
+filestem <- "LTER_survey-00_demographics_"
 
+## ------------------------------------- ##
+# Gender ----
+## ------------------------------------- ##
 
+# Prepare the data for plotting
+demo_sub <- survey_prep(df = demo, resp = "gender", grp = "cohort") %>% 
+  dplyr::mutate(gender = factor(gender, levels = c("Male", "Female",
+                                                   "Prefer To Self-Identify")))
 
+# Make desired graph
+ggplot(demo_sub, mapping = aes(x = cohort, y = perc_resp, fill = gender)) +
+  geom_bar(stat = "identity") +
+  labs(x = "Cohort", y = "Percent of Participants") +
+  geom_hline(yintercept = 50, linetype = 2) +
+  scale_fill_manual(values = c("#219ebc", "#ffb703", "#000")) +
+  lno_theme +
+  theme(legend.position = "top")
 
+# Generate nice file name
+(plotname <- paste0(filestem, "gender", ".png"))
 
-
-
-
-
-
+# Export the graph
+ggsave(filename = file.path("graphs", plotname), width = 6, height = 6, units = "in")
 
 # End ----

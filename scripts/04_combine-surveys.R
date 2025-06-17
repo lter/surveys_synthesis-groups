894e20816d77654918e2616524238ea5173f2a39
njlyon0 <nicholasjlyon@gmail.com>
2025-06-17 10:03:52 -0400

    feat: script skeleton for code to combine surveys (for questions asked more than once)

---
+++ scripts/04_combine-surveys.R
@@ -0,0 +1,33 @@
+## ---------------------------------------------------------- ##
+# Combine Surveys
+## ---------------------------------------------------------- ##
+
+# Purpose:
+## Some questions are asked in multiple surveys
+## By combining survey data, we can graph these to more explicitly identify change over time
+### Not just across cohorts (as is done by within-survey graphs)
+### But within them as well
+
+## ------------------------------------- ##
+# Housekeeping ----
+## ------------------------------------- ##
+
+# Load libraries
+librarian::shelf(tidyverse, supportR)
+
+# Clear environment
+rm(list = ls()); gc()
+
+## ------------------------------------- ##
+# Initial Wrangling ----
+## ------------------------------------- ##
+
+
+
+
+## ------------------------------------- ##
+# Export ----
+## ------------------------------------- ##
+
+
+# End ----

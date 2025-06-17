628255490dd1470ee073a9443f435172275c92ad
njlyon0 <nicholasjlyon@gmail.com>
2025-06-17 15:32:31 -0400

    feat (upload): use logical to determine whether all local graphs should get uploaded or just the ones not already in the Drive

--- scripts/06_upload-graphs.R
+++ scripts/06_upload-graphs.R
@@ -23,9 +23,26 @@ rm(list = ls()); gc()
 # Identify local graphs
 (local_graphs <- dir(path = file.path("graphs")))
 
+# Identify Drive folder where graphs are uploaded
+graph_drive_folder <- googledrive::as_id("https://drive.google.com/drive/u/0/folders/1m6vfzqW8a9c1AaalNugPaRDMda_9byxq")
+
+# Identify Drive graphs
+(drive_graphs <- googledrive::drive_ls(graph_drive_folder)$name)
+
+# Want to update Drive graphs by overwriting with local ones?
+update <- FALSE
+
+# Identify graphs to upload
+if(update == T){
+  wanted_graphs <- local_graphs
+} else {
+  wanted_graphs <- setdiff(x = local_graphs, y = drive_graphs)
+}
+
 # Upload them to the Drive (overwriting files of the same name)
-purrr::walk(.x = local_graphs,
-            .f = ~ googledrive::drive_upload(media = file.path("graphs", .x), overwrite = T,
-                                             path = googledrive::as_id("https://drive.google.com/drive/u/0/folders/1m6vfzqW8a9c1AaalNugPaRDMda_9byxq")))
+purrr::walk(.x = wanted_graphs,
+            .f = ~ googledrive::drive_upload(media = file.path("graphs", .x), 
+                                             overwrite = T,
+                                             path = graph_drive_folder))
 
 # End ----

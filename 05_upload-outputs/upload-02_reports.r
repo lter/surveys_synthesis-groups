0f0d6dff16fc3c6a9709765d3424ef70d839a907
njlyon0 <nicholasjlyon@gmail.com>
2026-03-30 10:22:33 -0400

    style: streamline drive folder identificatio

--- 05_upload-outputs/upload-02_reports.r
+++ 05_upload-outputs/upload-02_reports.r
@@ -22,9 +22,12 @@ rm(list = ls()); gc()
 # Identify local graphs
 (local_reports <- dir(pattern = "*.pdf"))
 
+# Identify Drive folder where reports are uploaded
+report_drive_folder <- googledrive::as_id("https://drive.google.com/drive/u/0/folders/1IDI3xruhkmhq__uXa-p9fCwCDPSM_G9x")
+
 # Upload them to the Drive (overwriting files of the same name)
 purrr::walk(.x = local_reports,
-            .f = ~ googledrive::drive_upload(media = .x, overwrite = T,
-                                             path = googledrive::as_id("https://drive.google.com/drive/u/0/folders/1IDI3xruhkmhq__uXa-p9fCwCDPSM_G9x")))
+  .f = ~ googledrive::drive_upload(media = .x, 
+    overwrite = T, path = report_drive_folder))
 
 # End ----
